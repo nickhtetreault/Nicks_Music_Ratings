cd35c953a2f3a968d7f665247b5b593364c917df
Nick Tetreault <nicholashtetreault@gmail.com>
2024-09-19 15:51:41 -0500

    Filtering unwanted albums from Spotify data

--- filter_items.py
+++ filter_items.py
@@ -43,4 +43,68 @@ ex. Artist: Opeth
 '''
 
 def filter_albums(album_objects):
-    return 0
+    # Checking for repeat releases (Remastered, Special Edition, etc.)
+    end = len(album_objects)
+    for i in range(1, end):
+        # Usually remastered album after original in list
+        if (album_objects[i - 1] in album_objects[i]):
+            # deleting repeats
+            del album_objects[i]
+            # moving back in array to check if there are multiple duplicates (ex. Opeth)
+            i -= 1
+            # dynamically change ending index for range of for loop, prevent index out of bounds
+            end -= 1
+    
+    # Checking for live and compilation albums, cleaning remaining album and song titles
+    for album in album_objects:
+        if ("live" in album.album_title.lower()):
+            del album
+            continue
+        if (check_comp(album.album_title)):
+            del album
+            continue
+        clean_alb_title(album)
+        for song in album.song_titles:
+            clean_song_title(song)
+
+def check_comp(title):
+    remove = [
+        "best of",
+        "presents",
+        "compilation",
+        "greatest hits",
+        "singles collection",
+        "collection",
+    ]
+    return (r in title.lower() for r in remove)
+
+
+def clean_alb_title(album):
+    remove = [
+        "(Remastered)",
+        "Remastered",
+        "(Remaster)",
+        "Remaster",
+        "(Special Edition)",
+        "Special Edition",
+        f"({album.release_date} Remaster)",
+        f"{album.release_date} Remaster",
+    ]
+    for r in remove:
+        pos = album.album_title.find(r)
+        if (pos > -1):
+            album.abum_title[0:pos].strip()
+
+def clean_song_title(title):
+    remove = [
+        "(Remastered)",
+        "Remastered",
+        "(Remaster)",
+        "Remaster",
+        "(Special Edition)",
+        "Special Edition",
+    ]
+    for r in remove:
+        pos = title.find(r)
+        if (pos > -1):
+            title[0:pos].strip()
