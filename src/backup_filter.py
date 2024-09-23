c4e4a823b68b6e751ba46fb93b248d0dc8ad895d
Nick Tetreault <nicholashtetreault@gmail.com>
2024-09-22 23:51:32 -0500

    finished implementing backup_filter

--- src/backup_filter.py
+++ src/backup_filter.py
@@ -2,12 +2,25 @@ import google.generativeai as genai
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+   
+def get_gemini_data(artist_name):
+    load_dotenv()
+    key = os.getenv("API_KEY")
+    genai.configure(api_key=key)
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content(f"Get the titles of every core studio albums by artist {artist_name}. Return this in the form of a list of strings to be parsed in python.")
+    print(response.text)
+    data = response.text
+    # cleaning respons to be list of album titles
+    albs = data[(data.find("[") + 1):data.find("]")].replace("\"", "").split(",")
 
-key = os.getenv("API_KEY")
+    # removing whitespace
+    for i in range(len(albs)):
+        albs[i] = albs[i].strip()
 
-genai.configure(api_key=key)
+    return albs
 
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Get the titles of every core studio albums by artist The Mars Volta. Return this in the form of a list of strings to be parsed in python.")
-print(response.text)
+albs = get_gemini_data("Led Zeppelin")
+
+for alb in albs:
+    print(alb)
