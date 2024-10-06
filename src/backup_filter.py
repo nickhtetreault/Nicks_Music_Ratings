4bec3c4fd7d961deef6c169f427aaf1984289610
Nick Tetreault <nicholashtetreault@gmail.com>
2024-10-06 16:08:26 -0500

    Improved prompting and switched to returning indices

--- src/backup_filter.py
+++ src/backup_filter.py
@@ -1,26 +1,38 @@
 import google.generativeai as genai
 from dotenv import load_dotenv
+from artist_data import Artist
+from artist_data import *
 import os
 
    
-def get_gemini_data(artist_name):
+def get_gemini_data(artist):
     load_dotenv()
     key = os.getenv("API_KEY")
     genai.configure(api_key=key)
     model = genai.GenerativeModel("gemini-1.5-flash")
-    response = model.generate_content(f"Get the titles of every core studio albums by artist {artist_name}. Return this in the form of a list of strings to be parsed in python.")
+    album_titles = []
+    for alb in artist.album_objects:
+        album_titles.append(alb.album_title)
+    print(album_titles)
+    print("break\n\n\n\n")
+    response = model.generate_content(f"Here is a list of albums released by {artist.artist_name}: {album_titles}. Return a list of the indices of albums in the provided list which are not studio albums (i.e. compilations, live releases, remixes, etc.). Do not add any albums that were not provided, and only respond with a list of indices which can be parsed in python.")
     print(response.text)
     data = response.text
-    # cleaning respons to be list of album titles
-    albs = data[(data.find("[") + 1):data.find("]")].replace("\"", "").split(",")
+    # cleaning response to be list of album titles
+    indices = data[(data.find("[") + 1):data.find("]")].replace("\"", "").split(",")
 
     # removing whitespace
-    for i in range(len(albs)):
-        albs[i] = albs[i].strip()
+    for i in range(len(indices)):
+        indices[i] = indices[i].lower().strip()
 
-    return albs
+    return indices
 
-albs = get_gemini_data("Led Zeppelin")
+token = get_token()
+temp = Artist(token, "Dream Theater")
+remove = get_gemini_data(temp)
 
-for alb in albs:
-    print(alb)
+# for num in remove:
+
+
+# for alb in remove:
+#     print(alb)
